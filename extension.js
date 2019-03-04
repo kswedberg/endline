2b9961ae878d228aaea75f2e3c270846bbd6ff46
Karl Swedberg <kswedberg@gmail.com>
2019-03-03 21:08:22 -0500

    Update actions

--- extension.js
+++ extension.js
@@ -1,23 +1,49 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-const {contributes} = require('./package.json');
-const {commands} = contributes;
+const pkg = require('./package.json');
+const {commands} = pkg.contributes;
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
 const endline = (cmd) => {
-  vscode.window.showInformationMessage(cmd.title);
-  console.log(cmd.title);
+  // vscode.window.showInformationMessage(cmd.title);
+  const activeEditor = vscode.window.activeTextEditor;
+
+  if (!activeEditor) {
+    return;
+  }
+  const rStopChars = /[{(,;]/;
+
+  vscode.commands.executeCommand('acceptSelectedSuggestion')
+  .then(() => {
+    const name = cmd.command.replace(/^endline\./, '');
+    const command = name === 'end' ? 'cursorEnd' : 'editor.action.insertLineAfter';
+    const {line: index} = activeEditor.selection.active;
+    const line = activeEditor.document.lineAt(index);
+    const len = line.text.replace(/\s+$/, '').length;
+    const punctuation = ';';
+
+    if (rStopChars.test(line.text.charAt(len - 1)) || line.isEmptyOrWhitespace) {
+      return name === 'stay' ? false : vscode.commands.executeCommand(command);
+    }
+
+    const inserted = activeEditor.edit((editBuilder) => {
+      editBuilder.insert(new vscode.Position(index, len), punctuation);
+    });
+
+    if (!inserted || name === 'stay') {
+      return;
+    }
+
+    return vscode.commands.executeCommand(command);
+  });
 };
+
 const activate = (context) => {
-
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "endline" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
+  // The commands have been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   commands.forEach((cmd) => {
     const disposable = vscode.commands.registerCommand(cmd.command, () => {
@@ -26,13 +52,6 @@ const activate = (context) => {
 
     context.subscriptions.push(disposable);
   });
-  // vscode.commands.registerCommand('extension.helloWorld', () => {
-  //   // The code you place here will be executed every time your command is executed
-
-  //   // Display a message box to the user
-  //   vscode.window.showInformationMessage('Hello World!');
-  // });
-
 };
 
 exports.activate = activate;
